d152dc2de11c0c6a37bc63f526095eca9ab69132
xu.hongrui <xu.hongrui@uneed.com>
2022-03-16 16:55:27 +0800

    feat:增加虚拟列表范例

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+ <div class="container">
+    <my-virtual-scroller :items="list" />
+  </div>
+</template>
+<script>
+import myVirtualScroller from './components/virtualScoller.vue'
+// 模拟一个长列表
+const list = []
+for(let i=0; i<10000; i++) {
+  list.push({
+    id: i,
+    label: `virtual-list ${i}`
+  })
+}
+export default {
+  components: {
+    myVirtualScroller
+  },
+  data() {
+    return {
+      list: list
+    }
+  }
+}
+</script>
+
+
+<style>
+.container {
+  height: 1000px;
+  border: 1px solid #ccc;
+}
+</style>
